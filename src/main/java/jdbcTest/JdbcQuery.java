6d53be3468d7c1de758c753cc04b69d2a3322a9c
enver <enverkaraca.wise@gmail.com>
2023-09-11 19:51:34 +0200

    first commit

---
+++ src/main/java/jdbcTest/JdbcQuery.java
@@ -0,0 +1,17 @@
+package jdbcTest;
+
+import java.nio.channels.ConnectionPendingException;
+
+public class JdbcQuery {
+    public static <Connection> void main(String[] args) throws ClassNotFoundException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+
+        Connection DriverManager;
+
+    }
+
+
+
+}
